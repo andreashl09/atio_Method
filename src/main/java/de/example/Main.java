11d246d67c8328a6ee9d1cc2d5469bd703705126
andreashl09 <a9279352@gmail.com>
2024-07-26 23:49:20 +0200

    method done

--- src/main/java/de/example/Main.java
+++ src/main/java/de/example/Main.java
@@ -2,6 +2,25 @@ package de.example;
 
 public class Main {
     public static void main(String[] args) {
-        String
+        String s = "    -6234445cde79";
+
+        System.out.println(myAtoi(s));
+    }
+
+    public static int myAtoi(String s) {
+        if (s.isEmpty()) return 0;
+
+        int output = 0;
+        int signum = 1;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isLetter(c)) return output * signum;
+            signum = (c == 45) ? -1 : signum;
+            if (Character.isDigit(c)) {
+                int digit = c - '0';
+                output = output * 10 + digit;
+            }
+        }
+        return output * signum;
     }
 }
